19db330622337214599fae266e8f5c41cd574fd9
jpcchaves <jpcchaves@outlook.com>
2026-04-15 18:24:08 -0300

    first commit

---
+++ src/test/java/br/com/springcache/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springcache.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
