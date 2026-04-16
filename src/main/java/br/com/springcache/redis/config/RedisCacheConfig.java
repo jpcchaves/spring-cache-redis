45729c5c70ce3274d17771f3a3abb4d592631cdc
jpcchaves <jpcchaves@outlook.com>
2026-04-15 22:20:05 -0300

    Add redis config

--- src/main/java/br/com/springcache/redis/config/RedisCacheConfig.java
+++ src/main/java/br/com/springcache/redis/config/RedisCacheConfig.java
@@ -1,11 +1,15 @@
 package br.com.springcache.redis.config;
 
+import br.com.springcache.redis.dto.CreditAnalysisResponseDTO;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -14,5 +18,24 @@ public class RedisCacheConfig {
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         return RedisCacheManager.create(redisConnectionFactory);
+    }
+
+    @Bean
+    public RedisTemplate<String, CreditAnalysisResponseDTO> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, CreditAnalysisResponseDTO> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        JacksonJsonRedisSerializer<CreditAnalysisResponseDTO> serializer =
+                new JacksonJsonRedisSerializer<>(CreditAnalysisResponseDTO.class);
+
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+
+        return template;
     }
 }
